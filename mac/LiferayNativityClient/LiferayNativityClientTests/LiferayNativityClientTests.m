7003d743508473cc76e78e8406c38de9f9128556
Charles Francoise <charles.francoise@forgetbox.com>
2014-02-05 11:42:54 +0100

    add test target for lib add JSONKit and GCDAsyncSocket

---
+++ mac/LiferayNativityClient/LiferayNativityClientTests/LiferayNativityClientTests.m
@@ -0,0 +1,34 @@
+//
+//  LiferayNativityClientTests.m
+//  LiferayNativityClientTests
+//
+//  Created by Charles Francoise on 05/02/14.
+//  Copyright (c) 2014 Charles Francoise. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LiferayNativityClientTests : XCTestCase
+
+@end
+
+@implementation LiferayNativityClientTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
