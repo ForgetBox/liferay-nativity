6e8747d160da23de769b8a4c12160b175688f013
Charles Francoise <charles.francoise@forgetbox.com>
2014-02-05 11:25:17 +0100

    add project for client lib + ignore .DS_Store

---
+++ mac/LiferayNativityClient/LiferayNativityClient.h
@@ -0,0 +1,13 @@
+//
+//  LiferayNativityClient.h
+//  LiferayNativityClient
+//
+//  Created by Charles Francoise on 05/02/14.
+//  Copyright (c) 2014 Charles Francoise. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LiferayNativityClient : NSObject
+
+@end
---
+++ mac/LiferayNativityClient/LiferayNativityClient.m
@@ -0,0 +1,13 @@
+//
+//  LiferayNativityClient.m
+//  LiferayNativityClient
+//
+//  Created by Charles Francoise on 05/02/14.
+//  Copyright (c) 2014 Charles Francoise. All rights reserved.
+//
+
+#import "LiferayNativityClient.h"
+
+@implementation LiferayNativityClient
+
+@end
