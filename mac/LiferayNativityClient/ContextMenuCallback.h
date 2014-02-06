548524a97009f7ddc4857b5efaf5dc80fc48a2e9
Charles Francoise <charles.francoise@forgetbox.com>
2014-02-06 19:14:48 +0100

    add files for Context Menu

---
+++ mac/LiferayNativityClient/ContextMenuCallback.h
@@ -0,0 +1,13 @@
+//
+//  ContextMenuCallback.h
+//  LiferayNativityClient
+//
+//  Created by Charles Francoise on 06/02/14.
+//  Copyright (c) 2014 Forgetbox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ContextMenuCallback : NSObject
+
+@end
---
+++ mac/LiferayNativityClient/ContextMenuCallback.m
@@ -0,0 +1,13 @@
+//
+//  ContextMenuCallback.m
+//  LiferayNativityClient
+//
+//  Created by Charles Francoise on 06/02/14.
+//  Copyright (c) 2014 Forgetbox. All rights reserved.
+//
+
+#import "ContextMenuCallback.h"
+
+@implementation ContextMenuCallback
+
+@end
